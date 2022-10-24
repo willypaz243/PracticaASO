ac943deea38a7f483a3a68490aa47c8ed1862c59
root <root@localhost.localdomain>
2022-10-24 12:29:11 -0400

    php instalacion

--- instalacion.sh
+++ instalacion.sh
@@ -1,2 +1,12 @@
 # Instalar apache2
 zypper install apache2
+# Instalar php7
+zypper install php7 apache2-mod_php7
+# enable php modules 
+a2enmod php7
+# copy php code to apache htdocs
+cp ./index.php /srv/www/htdocs/
+# enable apache2 service
+service apache2 stop
+service apache2 start
+
